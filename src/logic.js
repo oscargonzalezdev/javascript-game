0907dec49d26cf149404ce40a0df4f2fbe4df0d0
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-04-14 12:41:53 +0200

    solve move funtionality and add collition detection

--- src/logic.js
+++ src/logic.js
@@ -1,56 +1,108 @@
+/* ----------------------------- GAME CLASS ----------------------------- */
 class Game {
-    constructor(){
+    constructor() {
         this.diver = new Diver();
-        // this.opponent = new Opponent();
         this.gameInterval = null;
-        this.opponentsArray = []; // will store instances of the class Opponent
+        this.opponentsArray = []; // store instances of the class Opponent
+        this.score = 0;
         this.counter = 0;
     }
-    play (){
+    // executed when the player press play
+    play() {
         this.diver.showDiver();
         this.diver.moveDiver();
-        this.setup('fish');
-        this.setup('shark');
+        this.setup();
     }
-    setup(typeOpponent){
-        this.gameInterval = setInterval( () => {
-
-            // move and draw oponents
+    // this method manages what happens inside the interval
+    setup() {
+        this.gameInterval = setInterval(() => {
+            
+            // move and show oponents
             this.opponentsArray.forEach(opponent => {
                 opponent.moveOpponent();
                 opponent.showOpponent();
+                this.collision(opponent);
+                this.outside(opponent);
             });
 
-            // create new oponents
-            if (this.counter % 10 === 0){
+            // create oponents
+            if (this.counter % 50 === 0) {
                 const newOpponent = new Opponent();
-                // this.opponent.opponentElement.style.left = this.positionX;
-                newOpponent.createOpponent(typeOpponent);
+                newOpponent.createOpponent('fish');
                 this.opponentsArray.push(newOpponent);
-            } 
+            }
+            else if (this.counter % 40 === 0) {
+                const newOpponent = new Opponent();
+                newOpponent.createOpponent('shark');
+                this.opponentsArray.push(newOpponent);
+            }
+            else if (this.counter % 30 === 0) {
+                const newOpponent = new Opponent();
+                newOpponent.createOpponent('trash1');
+                this.opponentsArray.push(newOpponent);
+            }
+            else if (this.counter % 20 === 0) {
+                const newOpponent = new Opponent();
+                newOpponent.createOpponent('trash2');
+                this.opponentsArray.push(newOpponent);
+            }
             this.counter++;
-        }, 200);
+        }, 150);
+    }
+
+    // detects collisions between diver and opponents
+    collision(opponent) {
+        let diver = this.diver.diverElement.getBoundingClientRect(); // this method return a object whit information about size and position
+        let opp = opponent.opponentElement.getBoundingClientRect();
+
+        // check values of vertical and horizontal axis
+        if (diver.x < opp.x + opp.width && diver.x + diver.width > opp.x &&
+            diver.y < opp.y + opp.height && diver.height + diver.y > opp.y) {
+            switch (opponent.opponentElement.className) {
+                case 'shark':
+                    console.log('game over'); 
+                    clearInterval(this.gameInterval);            
+                    break;
+                case 'trash1':
+                case 'trash2':
+                    opponent.opponentElement.remove();
+                    setTimeout(() => {
+                        this.score += 100;
+                    }, 100);
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+    }
+    // check when elements are outside the game board
+    outside(opponent) {
+        if (opponent.positionX < 0) {
+            this.opponentsArray.shift();
+            opponent.opponentElement.remove();
+            // console.log('element is outside');
+        }
     }
 }
-// class to manage the diver
+/* ----------------------------- DIVER CLASS ----------------------------- */
 class Diver {
     constructor() {
-        this.width = 100;
-        this.height = 50;
+        this.width = 10;
+        this.height = 10;
         this.positionX = 0;
         this.positionY = 0;
         this.diverElement = null;
-        this.lives = null;
     }
     // this method shows and setup a new DOM element with class 'diver'
     showDiver() {
         const board = document.getElementById('game-board');
         this.diverElement = document.createElement('div');
         this.diverElement.className = 'diver';
-        this.diverElement.style.width = this.width + 'px';
-        this.diverElement.style.height = this.height + 'px';
-        this.diverElement.style.left = this.positionX +'%';
-        this.diverElement.style.top = this.positionY +'%';
+        this.diverElement.style.width = this.width + '%';
+        this.diverElement.style.height = this.height + '%';
+        this.diverElement.style.left = this.positionX + '%';
+        this.diverElement.style.top = this.positionY + '%';
         board.appendChild(this.diverElement);
     }
 
@@ -59,34 +111,32 @@ class Diver {
         window.addEventListener('keydown', (e) => {
             switch (e.key) {
                 case 'ArrowRight':
-                    this.diverElement.style.left = this.positionX++ + '%';
+                    this.diverElement.style.left = this.positionX++ * 2 + '%';
                     break;
                 case 'ArrowLeft':
-                    this.diverElement.style.left = this.positionX-- + '%';
+                    this.diverElement.style.left = this.positionX-- * 2 + '%';
                     break;
                 case ' ':
                 case 'ArrowDown':
-                    this.diverElement.style.top = this.positionY++ + '%';
+                    this.diverElement.style.top = this.positionY++ * 2 + '%';
                     break;
                 case 'ArrowUp':
-                    this.diverElement.style.top = this.positionY-- + '%';
+                    this.diverElement.style.top = this.positionY-- * 2 + '%';
                     break;
             }
         });
     }
-    // element moves up automatically
-    // autoDiving() {
-    //     this.positionY--;
-    // }
 }
 
+/* ----------------------------- OPPONENT CLASS ----------------------------- */
 class Opponent {
-    constructor(){
-        this.width = 50;
-        this.height = 50;
-        this.positionX = 0;
-        this.positionY = Math.floor(Math.random() * 90) +'%';
-        this.opponentElement = null; 
+    constructor() {
+        this.width = 10;
+        this.height = 10;
+        this.positionX = 100;
+        this.positionY = Math.floor(Math.random() * 90);
+        this.opponentElement = null;
+        this.counter = 0;
     }
 
     createOpponent(typeOpponent) {
@@ -96,15 +146,23 @@ class Opponent {
         board.appendChild(this.opponentElement);
     }
 
-    showOpponent(){
-        this.opponentElement.style.width = this.width + 'px';
-        this.opponentElement.style.height = this.height + 'px';
-        this.opponentElement.style.right = this.positionX +'px';
-        this.opponentElement.style.top = this.positionY +'%';
+    showOpponent() {
+        this.opponentElement.style.width = this.width + '%';
+        this.opponentElement.style.height = this.height + '%';
+        this.opponentElement.style.left = this.positionX + '%';
+        this.opponentElement.style.top = this.positionY + '%';
     }
 
-    moveOpponent(){
-        this.positionX++;
-        this.opponentElement.style.right = this.positionX;
+    moveOpponent() {
+        this.positionX--;
+        this.opponentElement.style.left = this.positionX;
+        setInterval(() => {
+            if (this.counter % 2 === 0) {
+                this.positionY += 1;
+            } else {
+                this.positionY -= 1;
+            }
+            this.counter++;
+        }, 1000);
     }
 }
